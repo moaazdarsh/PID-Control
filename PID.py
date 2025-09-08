79015f2ebbfa8c94d67ae120458db644f976cb88
moaazdarsh <mo3az.misara@gmail.com>
2025-09-08 17:48:48 +0300

    Comments

--- PID.py
+++ PID.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-surroundings = -12
-T = -11
-Tlog = [T]
+surrounding = -12 #Surrounding Temperature
+T = -11 #Current Temperature
+Tlog = [T] #Temperature Log
 
-E = []
-DesiredT = 37
+D = [] #Difference Log
+DesiredT = 37 #Desired Temperature
 K_p = 0.1
 K_i = 0.01
 K_d = 0.05
@@ -15,15 +15,15 @@ P, I, D = 0, 0, 0
 t_end = 200
 t = 0
 while t < t_end:
-    T += (surroundings - T) * 0.01
+    T += (surrounding - T) * 0.01 #Natural Cooling
     Tlog.append(T)
 
-    E.append(DesiredT - T)
+    D.append(DesiredT - T)
     
-    P = K_p * E[-1]
-    if len(E) > 1:
-        I = K_i * np.trapezoid(E)
-        D = K_d * (E[-1] - E[-2])
+    P = K_p * D[-1] # The P term is proportional to the current error
+    if len(D) > 1:
+        I = K_i * np.trapezoid(D) # The I term is proportional to the integral of the error
+        D = K_d * (D[-1] - D[-2]) # The D term is proportional to the derivative of the error
     
     T += P + I + D
 
